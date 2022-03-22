648a965ed49f3f7ada72a7c5fb459720480389bb
webbler <36542501+webbler@users.noreply.github.com>
2022-03-22 08:04:25 -0600

    Initialize firebase-integration

---
+++ lib/main_prod.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+import '/app/my_app.dart';
+import '/flavors/build_config.dart';
+import '/flavors/env_config.dart';
+import '/flavors/environment.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  EnvConfig prodConfig = EnvConfig(
+    appName: "Flutter GetX Template Prod",
+    baseUrl: "https://api.github.com",
+    shouldCollectCrashLog: true,
+  );
+
+  BuildConfig.instantiate(
+    envType: Environment.PRODUCTION,
+    envConfig: prodConfig,
+  );
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: "conf/.env");
+  var app = await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform(Environment.PRODUCTION),
+  );
+  FirebaseAnalytics analytics = FirebaseAnalytics.instanceFor(app: app);
+  analytics.logEvent(name: "CUSTOM_EVENT", parameters: {"VAL": "VALUE1"});
+  runApp(const MyApp());
+}
